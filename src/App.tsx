5eeaf96925708c3141bc9e93ac73395ae46cd53e
Victor Cruz <VicttorrHugo100@gmail.com>
2026-01-27 15:36:44 -0300

    refactor: restructure App component and enhance Cube functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import { Canvas, } from '@react-three/fiber'
+import { Canvas, type Vector3 } from '@react-three/fiber'
 import './App.css'
 
-const App = () => {
+export function App() {
   return (
     <Canvas>
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
+      <directionalLight position={[0, 0, 2]} />
+      <Cube position={[1, 0, 0]} color='orange' />
+      <Cube position={[-1, 0, 0]} color='green' />
+      <Cube position={[0, 1, 0]} color='red' />
+      <Cube position={[0, -1, 0]} color='blue' />
     </Canvas>
   )
 }
+
+type CubeProps = {
+  position: Vector3;
+  color: string;
+};
+
+export function Cube(props: CubeProps) {
+  return (
+    <mesh position={props.position}>
+      <boxGeometry />
+      <meshStandardMaterial color={"orange"} />
+    </mesh>
+  );
+}
+
 
 export default App
