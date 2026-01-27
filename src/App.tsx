8ccc5b24353a98a64e10092fcac1ec003950ebfa
Victor Cruz <VicttorrHugo100@gmail.com>
2026-01-27 16:10:58 -0300

    refactor: enhance Cube component with size prop and add rotation animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { Canvas, type Vector3 } from '@react-three/fiber'
+import { Canvas, useFrame, type Vector3 } from '@react-three/fiber';
+import type * as THREE from "three";
 import './App.css'
+import { useRef } from 'react';
 
 export function App() {
   return (
     <Canvas>
-      <directionalLight position={[0, 0, 2]} />
-      <Cube position={[1, 0, 0]} color='orange' />
-      <Cube position={[-1, 0, 0]} color='green' />
-      <Cube position={[0, 1, 0]} color='red' />
-      <Cube position={[0, -1, 0]} color='blue' />
+      <directionalLight position={[0, 0, 2]} intensity={0.5} />
+      <ambientLight intensity={0.1} />
+
+      <group position={[0, -1, 0]}>
+        {/* <Cube position={[1, 0, 0]} color='orange' size={[1, 1, 1]} />
+        <Cube position={[-1, 0, 0]} color='green' size={[1, 1, 1]} />
+        <Cube position={[0, 1, 0]} color='red' size={[1, 1, 1]} />
+        <Cube position={[0, -1, 0]} color='blue' size={[1, 1, 1]} /> */}
+        <Cube position={[0, 0, 0]} color='blue' size={[1, 1, 1]} />
+      </group>
+
     </Canvas>
   )
 }
@@ -16,13 +24,22 @@ export function App() {
 type CubeProps = {
   position: Vector3;
   color: string;
+  size: [number, number, number],
 };
 
 export function Cube(props: CubeProps) {
+  const ref = useRef<THREE.Mesh>(null)
+
+  useFrame((state, delta) => {
+    if(ref.current){
+      ref.current.rotation.x += delta * 2
+    } 
+  });
+
   return (
-    <mesh position={props.position}>
-      <boxGeometry />
-      <meshStandardMaterial color={"orange"} />
+    <mesh ref={ref} position={props.position}>
+      <boxGeometry args={props.size} />
+      <meshStandardMaterial color={props.color} />
     </mesh>
   );
 }
