2f1e004e68bc9d3ef8254f00f84c6216fd429ed7
Victor Cruz <VicttorrHugo100@gmail.com>
2026-01-28 15:52:39 -0300

    feat: add Sphere and Torus components with interactive animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Canvas, useFrame, type Vector3 } from '@react-three/fiber';
 import type * as THREE from "three";
 import './App.css'
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 export function App() {
   return (
@@ -10,11 +10,10 @@ export function App() {
       <ambientLight intensity={0.1} />
 
       <group position={[0, -1, 0]}>
-        {/* <Cube position={[1, 0, 0]} color='orange' size={[1, 1, 1]} />
-        <Cube position={[-1, 0, 0]} color='green' size={[1, 1, 1]} />
-        <Cube position={[0, 1, 0]} color='red' size={[1, 1, 1]} />
-        <Cube position={[0, -1, 0]} color='blue' size={[1, 1, 1]} /> */}
-        <Cube position={[0, 0, 0]} color='blue' size={[1, 1, 1]} />
+        {/* <Cube position={[1, 0, 0]} color='red' size={[1, 1, 1]} /> */}
+        <Sphere position={[1, 0, 0]} color='blue' radius={0.6} segments={32} />
+        {/* <Torus position={[5, 0, 0]} color="yellow" radius={0.6} radialSegments={16} tube={0.1} tubularSegments={32} />
+        <TorusKnot position={[1, 0, 0]} color="yellow" radius={0.6} radialSegments={16} tube={0.1} tubularSegments={32} p={15} q={18} /> */}
       </group>
 
     </Canvas>
@@ -44,6 +43,100 @@ export function Cube(props: CubeProps) {
       <meshStandardMaterial color={props.color} />
     </mesh>
   );
+}
+
+
+type SphereProps = {
+  position: Vector3;
+  color: string;
+  radius: number;
+  segments: number;
+}
+
+export function Sphere(props: SphereProps) {
+
+  const ref = useRef<THREE.Mesh>(null)
+  const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  useFrame((state, delta) => {
+    if (ref.current) {
+      const speed = isHovered ? 1 : 0.2;
+      ref.current.rotation.y += delta * speed;
+    }
+  });
+
+  return (
+    <mesh
+      ref={ref}
+      position={props.position}
+      onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))}
+      onPointerLeave={() => setIsHovered(false)}
+      onPointerDown={() => setIsClicked(!isClicked)}
+      scale={isClicked ? 1.5 : 1}
+    >
+      <sphereGeometry args={[props.radius, props.segments, props.segments]} />
+      <meshStandardMaterial color={isHovered ? "orange" : "lightblue"} wireframe />
+    </mesh>
+  )
+}
+
+type TorusProps = {
+  position: Vector3;
+  color: string;
+  radius: number,
+  tube: number,
+  radialSegments: number,
+  tubularSegments: number;
+  arc?: number;
+}
+export function Torus(props: TorusProps) {
+  const ref = useRef<THREE.Mesh>(null)
+
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x += delta;
+      ref.current.rotation.y += delta;
+
+      ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2
+    }
+  })
+  return (
+    <mesh ref={ref} position={props.position}>
+      <torusGeometry args={[props.radius, props.tube, props.radialSegments, props.tubularSegments]} />
+      <meshStandardMaterial color={props.color} />
+    </mesh>
+  )
+}
+
+type TorusKnotProps = {
+  position: Vector3;
+  color: string;
+  radius: number,
+  tube: number,
+  radialSegments: number,
+  tubularSegments: number;
+  p: number;
+  q: number;
+
+}
+export function TorusKnot(props: TorusKnotProps) {
+  const ref = useRef<THREE.Mesh>(null)
+
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x += delta;
+      ref.current.rotation.y += delta;
+
+      ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2
+    }
+  })
+  return (
+    <mesh ref={ref} position={props.position}>
+      <torusKnotGeometry args={[props.radius, props.tube, props.radialSegments, props.tubularSegments, props.p, props.q]} />
+      <meshStandardMaterial color={props.color} />
+    </mesh>
+  )
 }
 
 
